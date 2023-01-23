5c9bbfdebfee1c07d83f5ee54addf433432b9bae
xnd-dev <xnd.dev@gmail.com>
2023-01-22 23:15:48 -0300

    refactor: creation of tokens index

--- src/lib/stitches/stitches-config.ts
+++ src/lib/stitches/stitches-config.ts
@@ -1,10 +1,12 @@
 import { createStitches, defaultThemeMap } from '@stitches/react'
-import { colors } from '../../themes/colors'
-import { fontSizes } from '../../themes/font-sizes'
-import { fontWeights } from '../../themes/font-weights'
-import { lineHeights } from '../../themes/line-heights'
-import { radii } from '../../themes/radii'
-import { space } from '../../themes/space'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '../../themes'
 
 export const {
   config,
