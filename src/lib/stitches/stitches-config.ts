87493af0f55082453f27792791eb0f5a4b55d86f
xnd-dev <xnd.dev@gmail.com>
2023-01-22 23:10:36 -0300

    feat: add tokens

--- src/lib/stitches/stitches-config.ts
+++ src/lib/stitches/stitches-config.ts
@@ -1,5 +1,10 @@
-import { createStitches } from '@stitches/react'
+import { createStitches, defaultThemeMap } from '@stitches/react'
 import { colors } from '../../themes/colors'
+import { fontSizes } from '../../themes/font-sizes'
+import { fontWeights } from '../../themes/font-weights'
+import { lineHeights } from '../../themes/line-heights'
+import { radii } from '../../themes/radii'
+import { space } from '../../themes/space'
 
 export const {
   config,
@@ -11,7 +16,18 @@ export const {
   theme,
   createTheme,
 } = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
   theme: {
     colors,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space,
   },
 })
